48d801a12466287cc5388bc5ba662e9148232e75
Henry He <119676880+HenryHe0123@users.noreply.github.com>
2024-06-12 00:51:43 +0000

    feat: add eval result for llava-realigned-v4

--- LLaVA/move.sh
+++ LLaVA/move.sh
@@ -1,12 +1,12 @@
-# 查找替换 llava-origin
+# 查找替换 llava-realigned-v4
 
 # 创建目标目录，如果它不存在
-mkdir -p eval_results/llava-origin
+mkdir -p eval_results/llava-realigned-v4
 
 # 移动文件到目标目录
-mv test_llava-13b.jsonl eval_results/llava-origin/test_llava-13b.jsonl
-mv test_llava-13b_result.json eval_results/llava-origin/test_llava-13b_result.json
-mv test_llava-13b_output.json eval_results/llava-origin/test_llava-13b_output.json
+mv test_llava-13b.jsonl eval_results/llava-realigned-v4/test_llava-13b.jsonl
+mv test_llava-13b_result.json eval_results/llava-realigned-v4/test_llava-13b_result.json
+mv test_llava-13b_output.json eval_results/llava-realigned-v4/test_llava-13b_output.json
 
 # 小心！
 # rm test_llava-13b-chunk*.jsonl
--- LLaVA/scripts/sqa_eval_batch.sh
+++ LLaVA/scripts/sqa_eval_batch.sh
@@ -3,7 +3,7 @@
 CHUNKS=8
 for IDX in {0..7}; do
     CUDA_VISIBLE_DEVICES=$IDX python -m llava.eval.model_vqa_science \
-        --model-path /data2/yhhe/code/LLaVA/checkpoints/llava-vicuna-13b-v1.3-origin-pretrain_lcs558k_plain-ScienceQA_QCM_LEA-12e \
+        --model-path /data2/yhhe/code/LLaVA/checkpoints/llava-vicuna-13b-v1.3-reformat-v4-12e \
         --question-file /data2/yhhe/code/ScienceQA/data/scienceqa/llava_test_QCM-LEA.json \
         --image-folder /data2/yhhe/code/ScienceQA/data/scienceqa/images/test \
         --answers-file ./test_llava-13b-chunk$CHUNKS_$IDX.jsonl \
